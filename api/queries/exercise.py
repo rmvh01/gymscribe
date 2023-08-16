691a3b4d6d82c824c52cf59d96f49525b338978c
Thanh Le <thanhle7271@gmail.com>
2023-08-16 10:25:18 -0700

    list and delete exercise

--- api/queries/exercise.py
+++ api/queries/exercise.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
 from queries.pool import pool
+from typing import List, Union
 
 
 class ExerciseIn(BaseModel):
@@ -41,3 +42,42 @@ class ExerciseRepo:
                 id = result.fetchone()[0]
                 old_data = exercise.dict()
                 return ExerciseOut(id=id, **old_data)
+
+    def get_all_exercise(self):
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as cur:
+                    cur.execute(
+                        """
+                        SELECT id, name, description, user_id
+                        FROM exercises;
+                        """,
+                    )
+                    result = cur.fetchall()
+                    exercise = [
+                        ExerciseOut(
+                            id=row[0],
+                            name=row[1],
+                            description=row[2],
+                            user_id=row[3]
+                        )
+                        for row in result
+                    ]
+                    return exercise
+        except Exception:
+            return {"message": "cannot get all exercise"}
+
+    def delete_exercise(self, exercise_id:int):
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as cur:
+                    cur.execute(
+                        """
+                        DELETE FROM exercises
+                        WHERE id = %s;
+                        """,
+                        [exercise_id],
+                    )
+                    return {"message": "Exercise deleted successfully!"}
+        except Exception:
+            return {"message": "Failed to delete exercise"}
--- api/routers/exercise.py
+++ api/routers/exercise.py
@@ -2,8 +2,9 @@ from fastapi import (
     APIRouter,
     Depends
 )
-from pydantic import BaseModel
-from queries.exercise import ExerciseRepo, ExerciseIn
+
+from queries.exercise import ExerciseRepo, ExerciseIn, ExerciseOut
+from typing import List
 
 
 router = APIRouter()
@@ -16,4 +17,21 @@ def create_exercise(
 ):
     return repo.create_exercise(
         exercise
+    )
+
+
+@router.get("/api/exercise", response_model=List[ExerciseOut])
+def get_exercise(
+    repo: ExerciseRepo = Depends(),
+):
+    return repo.get_all_exercise()
+
+
+@router.delete("/api/exercise/{exercise_id}", response_model=dict)
+def delete_exercise(
+    exercise_id: int,
+    repo: ExerciseRepo = Depends(),
+):
+    return repo.delete_exercise(
+        exercise_id
     )
