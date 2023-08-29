b4ff27275635a94b5531cf620ff1efc7cd9653f4
max <rmvh01@proton.me>
2023-08-29 10:18:38 -0500

    create_exercise page started, workoutexerciseslist started but needs to be reworked

--- api/queries/workout_exercises.py
+++ api/queries/workout_exercises.py
@@ -13,24 +13,31 @@ class WorkoutExerciseOut(WorkoutExerciseIn):
 
 class WorkoutExercisesRepo:
 
-    def add_exercise_to_workout(self, workout_exercise: WorkoutExerciseIn):
+    def add_exercise_to_workout(
+            self,
+            workout_exercise: WorkoutExerciseIn
+    ):
         with pool.connection() as conn:
             with conn.cursor() as cur:
                 result = cur.execute(
                     """
                     INSERT INTO workout_exercises
-                        (workout_id, exercise_id)
+                        (workout_id,
+                        exercise_id)
                     VALUES
                     (%s, %s)
                     RETURNING id;
                     """,
                     [
                         workout_exercise.workout_id,
-                        workout_exercise.exercise_id
+                        workout_exercise.exercise_id,
                     ],
                 )
                 id = result.fetchone()[0]
-                return WorkoutExerciseOut(id=id, **workout_exercise.dict())
+                return WorkoutExerciseOut(
+                    id=id,
+                    **workout_exercise.dict()
+                )
 
     def get_exercises_for_workout(self, workout_id: int):
         with pool.connection() as conn:
@@ -45,15 +52,15 @@ class WorkoutExercisesRepo:
                 )
                 return [row[0] for row in cur.fetchall()]
 
-    def remove_exercise_from_workout(self, id: int):
+    def remove_exercise_from_workout(self, exercise_id: int, workout_id: int):
         with pool.connection() as conn:
             with conn.cursor() as cur:
                 cur.execute(
                     """
                     DELETE FROM workout_exercises
-                    WHERE id = %s;
+                    WHERE workout_id = %s AND exercise_id = %s;
                     """,
-                    [id]
+                    [exercise_id, workout_id]
                 )
                 return (
                     {"message": "Exercise removed from workout successfully!"}
--- ghi/src/workouts/WorkoutForm.js
+++ ghi/src/workouts/WorkoutForm.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Form, useNavigate } from "react-router-dom";
 import useToken from "@galvanize-inc/jwtdown-for-react";
 
