0207f33d9ada33c938621f3c0acfa98c8e8ace60
max <rmvh01@proton.me>
2023-08-29 12:52:11 -0500

    updated workoutexerciseslist.js so it works now

--- ghi/src/workouts/WorkoutExercisesList.js
+++ ghi/src/workouts/WorkoutExercisesList.js
@@ -2,104 +2,121 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import useToken from "@galvanize-inc/jwtdown-for-react"; // Uncomment if you use this
 
-function ExercisesList() {
+// we need:
+// one useState for the list of exercises, this should be filtered by user_id, and against the exercises already in the workout
+// a second useState for the list of exercises in the workout
+
+function WorkoutExercisesList() {
+  // const token, and useState for (exercises not in workout) and one for (exercises in the workout)
   const { token } = useToken();
-  const [exercises, setExercises] = useState([]);
-  const [workoutExercises, setWorkoutExercises] = useState([]);
   const { workout_id } = useParams();
+  const [exercisesNotInWorkout, setExercisesNotInWorkout] = useState([]);
+  const [exercisesInWorkout, setExercisesInWorkout] = useState([]);
+  const [forceRefresh, setForceRefresh] = useState(false);
+  // workout_id from use_params to get user_id from get one workout query
 
-  // i can fetch the workout data to get the user_id
+  // this block is for initial population
 
+  // fetch workout_exercise data: a list of exercise_ids
   const fetchData = async () => {
-    // this try is to get the user_id
-    try {
-      const response = await fetch(
-        `${process.env.REACT_APP_API_HOST}/api/exercises`
-      );
-      if (response.ok) {
-        // needs to only be exercises for a given user
-        const workoutByIdResponse = await fetch(
-          `${process.env.REACT_APP_API_HOST}/api/workout/${workout_id}`
-        );
-        if (workoutByIdResponse.ok) {
-          const workoutByIdJson = await workoutByIdResponse.json();
-          const usr_id = workoutByIdJson.user_id;
-          const jsonData = await response.json();
-          console.log(jsonData[0].user_id);
-          const filteredExercises = jsonData.filter(
-            (exercise) => exercise.user_id === usr_id
-          );
-          console.log("jsonData from fetchData:", jsonData);
-          console.log("filtered jsondata", filteredExercises); // list of objects
-          // now for each of these exercises, we need to add the exercise data
-          fetchWorkoutExercises(filteredExercises);
-          // const twiceFiltered = workoutExercises.filter((exercise) => {
-          //   return !workoutExercises.some(
-          //     (existing_exercise) => existing_exercise.id === exercise.id
-          //   );
-          // });
-          // setExercises(twiceFiltered);
-        }
+    const filteredExerciseData = await fetchExerciseData();
+    const exerciseIdList = await fetchWorkoutExerciseIds(workout_id);
+    console.log(exerciseIdList);
+    const exercisesIn = [];
+    const exercisesOut = [];
+    console.log(filteredExerciseData);
+    for (let exercise of filteredExerciseData) {
+      if (exerciseIdList.includes(exercise.id)) {
+        exercisesIn.push(exercise);
+      } else {
+        exercisesOut.push(exercise);
       }
-    } catch (error) {
-      console.error("Error fetching data:", error);
+      console.log(exercisesOut);
+      console.log(exercisesIn);
+      setExercisesInWorkout(exercisesIn);
+      setExercisesNotInWorkout(exercisesOut);
     }
   };
 
-  const fetchWorkoutExercises = async (filteredExercises) => {
+  useEffect(() => {
+    fetchData();
+  }, [forceRefresh]);
+
+  // fetch exercise data: a list of exercise objects filtered by user_id
+
+  // filter that exercise data for the user,
+
+  const fetchWorkoutExerciseIds = async (workout_id) => {
     try {
       const workoutExercisesResponse = await fetch(
         `${process.env.REACT_APP_API_HOST}/api/workout/${workout_id}/exercises`
       );
       if (workoutExercisesResponse.ok) {
-        const workoutExercisesJson = await workoutExercisesResponse.json(); // list of ids
-        // this is where i update workout exercises json with exercises data
-        const newWorkoutExercises = workoutExercisesJson.map((id) => {
-          const relatedExercise = filteredExercises.find(
-            (exercise) => exercise.id === id
-          );
-          return relatedExercise || id;
-        });
-
-        console.log("THISSSSS", newWorkoutExercises);
-        setWorkoutExercises(newWorkoutExercises);
-        // Filter the workoutExercises again
-        const twiceFiltered = newWorkoutExercises.filter((exercise) => {
-          return !newWorkoutExercises.some(
-            (existing_exercise) => existing_exercise.id === exercise.id
-          );
-        });
-        console.log("this::::", twiceFiltered);
-        setExercises(twiceFiltered);
-        console.log(workoutExercises);
+        const workoutExercisesJson = await workoutExercisesResponse.json();
+        return workoutExercisesJson; // this json is a list of exercise_ids that are in the workout
       } else {
-        console.log("failed to fetch workout_exercises");
+        return [];
       }
-    } catch (error) {
-      console.log("error fetching workout exercises");
+    } catch {
+      console.log("failed to fetch workout exercises id list");
+      return [];
     }
   };
 
-  const newWorkoutID = parseInt(workout_id);
-  const postExerciseToWorkoutExercises = async (exerciseToAdd) => {
-    const addedExerciseData = exercises.find((e) => e.id === exerciseToAdd.id);
-    console.log("added", addedExerciseData); // this has the exercise data
-    // Remove the exercise from exercise list
-    setExercises((oldExercises) => {
-      const newExercises = oldExercises.filter(
-        (e) => e.id !== exerciseToAdd.id
+  const fetchExerciseData = async () => {
+    try {
+      const exercisesResponse = await fetch(
+        `${process.env.REACT_APP_API_HOST}/api/exercises`
       );
-      return newExercises;
-    });
+      if (exercisesResponse.ok) {
+        const exercisesJson = await exercisesResponse.json();
+        // fitler that json for user_id
+        const user_id = await fetchUserId();
+        console.log(exercisesJson);
+        const newExercises = [];
+        for (let exercise of exercisesJson) {
+          console.log(user_id);
+          if (exercise.user_id === user_id) {
+            newExercises.push(exercise);
+          }
+        }
+        console.log(newExercises);
+        return newExercises; // these are only the user's exercises
+      }
+    } catch {
+      console.log("failed to fetch exercise data");
+    }
+  };
+
+  const fetchUserId = async () => {
+    try {
+      const workoutDetailResponse = await fetch(
+        `${process.env.REACT_APP_API_HOST}/api/workout/${workout_id}`
+      );
+      if (workoutDetailResponse.ok) {
+        const workoutDetailJson = await workoutDetailResponse.json();
+        const user_id = workoutDetailJson.user_id;
+        return user_id;
+      }
+    } catch {
+      console.log("failed to fetch workout details");
+    }
+  };
+
+  const postExerciseToWorkoutExercises = async (exercise) => {
+    // we have an exercise object in exercise
+    // we need to the workout_id and the exercise_id to post
     const url = `${process.env.REACT_APP_API_HOST}/api/workout_exercise`;
+    const newWorkoutId = parseInt(workout_id);
     const content = {
-      workout_id: newWorkoutID,
-      exercise_id: exerciseToAdd.id,
+      exercise_id: exercise.id,
+      workout_id: newWorkoutId,
     };
-    console.log("Posting the following content:", content); // Added console.log
+    const data = JSON.stringify(content);
+    console.log("content", content);
     const fetchConfig = {
       method: "POST",
-      body: JSON.stringify(content),
+      body: data,
       headers: {
         "Content-Type": "application/json",
         Authorization: `Bearer ${token}`,
@@ -107,36 +124,12 @@ function ExercisesList() {
     };
     const response = await fetch(url, fetchConfig);
     if (response.ok) {
-      console.log("Success!");
-      setWorkoutExercises((oldWorkoutExercises) => {
-        if (oldWorkoutExercises) {
-          const newWorkoutExerices = {
-            exercise_id: exerciseToAdd.id,
-            exerciseData: addedExerciseData,
-          };
-          console.log("newWorkoutExercises", newWorkoutExerices);
-          const newWorkoutExercises = [
-            ...oldWorkoutExercises,
-            newWorkoutExerices,
-          ];
-          return newWorkoutExercises;
-        } else {
-          const newWorkoutExercises = [
-            {
-              exercise_id: exerciseToAdd.id,
-              exercise_data: addedExerciseData,
-            },
-          ];
-          console.log("asdf");
-          return newWorkoutExercises;
-        }
-      });
+      console.log("Exercise posted to workout_exercises table");
+      setForceRefresh(true);
+    } else {
+      console.log("exercise could not be posted");
     }
   };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
 
   return (
     <div className="container mt-5 pt-1">
@@ -150,33 +143,19 @@ function ExercisesList() {
             </tr>
           </thead>
           <tbody className="table-group-divider">
-            {exercises &&
-              exercises.map((exercise) => {
-                // Filter out exercises that are already in workoutExercises
-                const exerciseNotInWorkout = !workoutExercises.some(
-                  (workoutExercise) =>
-                    workoutExercise.exercise_id === exercise.id
-                );
-                if (exerciseNotInWorkout) {
-                  return (
-                    <tr key={exercise.id + exercise.name}>
-                      <td>{exercise.name}</td>
-                      <td>{exercise.description}</td>
-                      <td>
-                        <button
-                          onClick={() =>
-                            postExerciseToWorkoutExercises(exercise)
-                          }
-                          className="btn btn-primary"
-                        >
-                          Add
-                        </button>
-                      </td>
-                    </tr>
-                  );
-                }
-                return null;
-              })}
+            {exercisesNotInWorkout.map((exercise) => (
+              <tr key={exercise.id}>
+                <td>{exercise.name}</td>
+                <td>{exercise.description}</td>
+                <td>
+                  <button
+                    onClick={() => postExerciseToWorkoutExercises(exercise)}
+                  >
+                    Add
+                  </button>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
         <h1 className="text-center mb-3">Exercises In Workout</h1>
@@ -188,24 +167,12 @@ function ExercisesList() {
             </tr>
           </thead>
           <tbody className="table-group-divider">
-            {workoutExercises === null ? (
-              <tr>
-                <td colSpan="2">Loading...</td>
+            {exercisesInWorkout.map((exercise) => (
+              <tr key={exercise.id}>
+                <td>{exercise.name}</td>
+                <td>{exercise.description}</td>
               </tr>
-            ) : (
-              workoutExercises &&
-              workoutExercises.map((workoutExercises, index) => {
-                const { exerciseData } = workoutExercises;
-                return (
-                  <tr key={"" + workoutExercises.id + index}>
-                    <td>{exerciseData ? exerciseData.name : "Loading..."}</td>
-                    <td>
-                      {exerciseData ? exerciseData.description : "Loading..."}
-                    </td>
-                  </tr>
-                );
-              })
-            )}
+            ))}
           </tbody>
         </table>
       </div>
@@ -213,4 +180,4 @@ function ExercisesList() {
   );
 }
 
-export default ExercisesList;
+export default WorkoutExercisesList;
