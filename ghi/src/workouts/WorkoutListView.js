14b718310af638eb13f933dbea3d4ab9d4b734d2
Eluron <elurono@gmail.com>
2023-08-30 11:28:32 -0500

    Working WorkoutListView.js

--- ghi/src/workouts/WorkoutListView.js
+++ ghi/src/workouts/WorkoutListView.js
@@ -6,6 +6,7 @@ function WorkoutListView() {
   const { token } = useToken();
   const navigate = useNavigate();
   const [workouts, setWorkouts] = useState([]);
+  const [exercises, setExercises] = useState([]);
 
   // Fetch workout data
   const fetchData = async () => {
@@ -31,31 +32,77 @@ function WorkoutListView() {
     fetchData();
   }, []);
 
+  useEffect(() => {
+    // Fetch all exercises associated with the user
+    const fetchExercises = async () => {
+      try {
+        const response = await fetch(
+          `${process.env.REACT_APP_API_HOST}/api/exercises`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        const exercisesData = await response.json();
+        setExercises(exercisesData);
+      } catch (error) {
+        console.log("Failed to fetch exercises: ", error);
+      }
+    };
+
+    fetchExercises();
+  }, []);
+
   return (
     <div className="container">
-      <h1>Workout List</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Description</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {workouts.map((workout) => (
-            <tr key={workout.id}>
-              <td>{workout.name}</td>
-              <td>{workout.description}</td>
-              <td>
-                <button onClick={() => navigate(`/workoutform/${workout.id}`)}>
-                  Edit
-                </button>
-              </td>
+      <div className="workout-section">
+        <h1>Workout List</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Description</th>
+              <th>Action</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {workouts.map((workout) => (
+              <tr key={workout.id}>
+                <td>{workout.name}</td>
+                <td>{workout.description}</td>
+                <td>
+                  <button
+                    onClick={() => navigate(`/workoutform/${workout.id}`)}
+                  >
+                    Edit
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div className="exercise-section">
+        <h1>Exercise List</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Description</th>
+            </tr>
+          </thead>
+          <tbody>
+            {exercises.map((exercise) => (
+              <tr key={exercise.id}>
+                <td>{exercise.name}</td>
+                <td>{exercise.description}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <button onClick={() => navigate("/exercises")}>Create Exercise</button>
+      </div>
     </div>
   );
 }
