7085f80456a8b10c91a77f5b93216187eeca7676
Eluron <elurono@gmail.com>
2023-08-30 10:33:44 -0500

    update

--- ghi/src/workouts/ShowOneWorkout.js
+++ ghi/src/workouts/ShowOneWorkout.js
@@ -1,21 +1,34 @@
 import React, { useState, useEffect } from "react";
-import { useParams, useNavigate } from "react-router-dom";
-import useToken from "@galvanize-inc/jwtdown-for-react";
+import { useParams, useNavigate, Link } from "react-router-dom";
 
 function ShowOneWorkout() {
   const { workout_id } = useParams();
   const [workoutdata, setWorkoutData] = useState({});
+  const navigate = useNavigate();
 
   const fetchData = async () => {
     const newWorkoutId = parseInt(workout_id);
     const url = `${process.env.REACT_APP_API_HOST}/api/workout/${workout_id}`;
     const response = await fetch(url);
     const json = await response.json();
-    console.log(json);
+    setWorkoutData(json);
   };
 
   useEffect(() => {
     fetchData();
   }, []);
+
+  return (
+    <div className="container">
+      <h1>{workoutdata.name}</h1>
+      <p>{workoutdata.description}</p>
+      <p>Date: {workoutdata.date}</p>
+      <Link to={`/workoutform/${workout_id}`}>Edit Workout</Link>
+      <button onClick={() => navigate(`/workout/${workout_id}/exercises`)}>
+        View Exercises
+      </button>
+    </div>
+  );
 }
+
 export default ShowOneWorkout;
