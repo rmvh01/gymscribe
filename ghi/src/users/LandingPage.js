401aae14cdfa0303491e2de12e9beb98065bb0fe
Eluron <elurono@gmail.com>
2023-09-06 14:10:04 -0500

    Landingpage updated css

--- ghi/src/users/LandingPage.js
+++ ghi/src/users/LandingPage.js
@@ -1,11 +1,19 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import "../styles.css"; // Assuming styles.css is in the same directory
 
 function LandingPage() {
-    return (
-        <div>
-            <h1>Welcome to Our Application!</h1>
-        </div>
-    );
+  return (
+    <div className="landing-container">
+      <h1 className="landing-title">Gymscribe</h1>
+      <p className="landing-subtitle">
+        Welcome to Gymscribe. Your favorite Workout Log. Login to get started.
+      </p>
+      <Link to="/login" className="landing-login-button-link">
+        <button className="landing-login-button">Login</button>
+      </Link>
+    </div>
+  );
 }
+
 export default LandingPage;
